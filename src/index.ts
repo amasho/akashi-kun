274f8481fb72dc34504b09a158ac65ae9c856b35
Shota Amano <amasho@dev-cycle.io>
2019-11-05 14:52:30 +0900

    fix: refactor

--- src/index.ts
+++ src/index.ts
@@ -1,44 +1,50 @@
 /**
  * usagee: yarn start [Company ID] [User ID] [Password]
  */
-import { Browser, Page, launch } from 'puppeteer'
+import { Browser, launch } from 'puppeteer'
 
 export class EngravingAkashi {
   private companyId: string = ''
   private userId: string = ''
   private password: string = ''
   private browser: Browser
-  private page: Page
 
   constructor(param: string[]) {
     this.companyId = param[2]
     this.userId = param[3]
     this.password = param[4]
+
+    launch({ headless: true }).then(o => {
+      this.browser = o
+      return this.main()
+    })
   }
 
   async main() {
-    this.browser = await launch()
-    this.page = await this.browser.newPage()
+    if (!this.companyId || !this.userId || !this.password) {
+      console.error('Error: parameter error. usage: yarn start [Company ID] [User ID] [Password]')
+      return
+    }
 
-    await this.page.goto('https://atnd.ak4.jp/login')
+    const page = await this.browser.newPage()
+    await page.setViewport({ width: 1200, height: 800 })
 
-    await this.page.type('input[id="form_company_id"]', this.companyId)
-    await this.page.type('input[id="form_login_id"]', this.userId)
-    await this.page.type('input[id="form_password"]', this.password)
+    await page.goto('https://atnd.ak4.jp/login')
 
-    this.page.click('input[type="submit"]')
+    await page.type('input[id="form_company_id"]', this.companyId)
+    await page.type('input[id="form_login_id"]', this.userId)
+    await page.type('input[id="form_password"]', this.password)
 
-    await this.page.waitForNavigation()
+    await page.click('input[type="submit"]')
 
-    await this.page.screenshot({ path: 'screenShotPage.png' })
+    await page.waitForSelector('a[data-punch-type="attendance"]')
+
+    await page.click('a[data-punch-type="attendance"]')
+
+    await page.screenshot({ path: 'screenShotPage.png' })
 
     await this.browser.close()
   }
 }
 
-if (!process.argv[2] || !process.argv[3] || !process.argv[4]) {
-  console.error('Error: parameter error. usage: yarn start [Company ID] [User ID] [Password]')
-} else {
-  const akashi = new EngravingAkashi(process.argv)
-  akashi.main()
-}
+new EngravingAkashi(process.argv)
